7f6e575842143f085d46ed45d1275e9fe1043737
Michael Lieberman <mlieberman85@gmail.com>
2024-02-08 00:43:04 -0500

    Fix REST API and logging
    This fixes some bugs in the REST API, specifically around the get
    projects functionality. This still needs to be updated to not just
    dump everything in all projects and return a list that can be
    iterated over to fetch elements for the projects, or at least
    have multiple calls for when you don't need a dump of all
    projects. This also follows some Actix best practices around
    logging. This introduces bunyan formatted logging.

--- skootrs-bin/src/main.rs
+++ skootrs-bin/src/main.rs
@@ -20,6 +20,11 @@ use skootrs_model::skootrs::SkootError;
 
 use helpers::{create, dump, get_facet};
 use tracing::error;
+use opentelemetry::global;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry};
+use tracing_subscriber::layer::SubscriberExt;
 
 
 #[derive(Parser)]
@@ -36,14 +41,37 @@ enum SkootrsCli{
     GetFacet,
 }
 
+fn init_tracing() {
+    let app_name = "skootrs";
+
+    // Start a new Jaeger trace pipeline.
+    // Spans are exported in batch - recommended setup for a production application.
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name(app_name)
+        .install_simple()
+        .expect("Failed to install OpenTelemetry tracer.");
+
+    // Filter based on level - trace, debug, info, warn, error
+    // Tunable via `RUST_LOG` env variable
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
+    // Create a `tracing` layer using the Jaeger tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    // Create a `tracing` layer to emit spans as structured logs to stdout
+    let formatting_layer = BunyanFormattingLayer::new(app_name.into(), std::io::stdout);
+    // Combined them all together in a `tracing` subscriber
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(telemetry)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to install `tracing` subscriber.")
+}
+
 #[tokio::main]
 async fn main() -> std::result::Result<(), SkootError> {
-    let subscriber = tracing_subscriber::fmt()
-    .with_file(true)
-    .with_line_number(true)
-    .finish();
-
-    tracing::subscriber::set_global_default(subscriber)?;
+    init_tracing();
     let cli = SkootrsCli::parse();
     let o: octocrab::Octocrab = octocrab::Octocrab::builder()
     .personal_token(
